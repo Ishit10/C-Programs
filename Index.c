b56b02f8614512e195c02030280db1d902768f84
ishit10 <kapuparaishit18@gmail.com>
2024-05-27 17:09:49 +0530

    my first commit

---
+++ Index.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+int main()
+{
+    // Qus 1
+
+    // int a,b;
+    // printf("Enter your num");
+    // scanf("%d %d",&a,&b);
+    // printf("your a is %d and your b is %d\n",a,b);
+    // int addition=a+b;
+    // int subtract=a-b;
+    // int multiplication=a*b;
+    // int division=a/b;
+    // printf("Addition is %d\n",addition);
+    // printf("Subtraction is %d\n",subtract);
+    // printf("Multiplication is %d\n",multiplication);
+    // printf("Division is %d\n",division);
+
+
+
+    // Qus 2
+
+    // int a,b,c;
+    // printf("Enter your int: ");
+    // scanf("%d %d %d",&a,&b,&c);
+    // printf("your a is %d\n your b is %d\n and your c is %d\n",a,b,c);
+    // int result=a+b*c-b/c;
+    // printf("Result is %d\n",result);
+
+
+    // Qus 3
+
+    // int a,b,c,d;
+    // printf("Enter your int: ");
+    // scanf("%d %d %d %d",&a,&b,&c,&d);
+    // printf("your a is %d your b is %d your c is %d your d is %d\n",a,b,c,d);
+    // int result=(a+b)*(c-d);
+    // printf("Result is %d\n",result);
+
+
+    // Qus 4
+
+    // int a,b,c,d,e;
+    // printf("Enter your int: ");
+    // scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    // printf("your a is %d your b is %d your c is %d your d is %d your e is %d",a,b,c,d,e);
+    // int result=a*b+c/d-e;
+    // printf("result is %d",result);
+
+
+    // Qus 5
+
+    // int length,width;
+    // printf("enter your length and width: ");
+    // scanf("%d %d",&length,&width);
+    // printf("your length is %d and your width is %d",length,width);
+    // int rectangle=length*width;
+    // printf("rectangle is %d",rectangle);
+
+
+    // Qus 6
+
+    // int length;
+    // printf("Enter youe length: ");
+    // scanf("%d",&length);
+    // printf("your length is %d",length);
+    // int square=length*length;
+    // printf("square is %d",square);
+
+
+    // Qus 7
+
+    // int p,r,t;
+    // printf("Enter your P, R, T: ");
+    // scanf("%d %d %d",&p,&r,&t);
+    // printf("your p is %d your r is %d your t is %d",p,r,t);
+    // int SI=(p*r*t)/100;
+    // printf("SI is %d",SI);
+
+
+    // Qus 8
+    // ERROR
+
+    // int salary;
+    // printf("Enter your salary: ");
+    // scanf("%d ",&salary);
+    // printf("salary is %d\n",salary);
+    // int HRA=20%salary;
+    // printf("HRA is %d\n",HRA);
+    // int DA=50%salary;
+    // printf("DA is %d\n",DA);
+    // int GS=salary+HRA+DA;
+    // printf("GS is %d",GS);
+    return 0;
+}
